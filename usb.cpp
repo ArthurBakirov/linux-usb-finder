cccd728626cadea8079a1c8b13407b469d35af59
arthur <arthur@debian11.myguest.virtualbox.org>
2022-12-04 14:19:00 +0300

    change syntax

--- usb.cpp
+++ usb.cpp
@@ -11,14 +11,13 @@
 #include <string>
 #include <vector>
 
-
-int main()
+void get_usb_storage_devname_list(std::vector<std::string>& device_devname_list)
 {
   sd_device* device;
   sd_device_enumerator* e;
   sd_device_enumerator_new(&e);
   const char *dev_name, *dev_type, *storage_type;
-  std::vector<std::string> device_devname_list;
+
   sd_device_enumerator_add_match_subsystem(e, "block", int(1));
   for(device = sd_device_enumerator_get_device_first(e);
       device != NULL; device = sd_device_enumerator_get_device_next(e))
@@ -27,29 +26,32 @@ int main()
     sd_device_get_devtype(device, &dev_type);
 
     if(dev_type == std::string("partition") &&
-	 storage_type == std::string("usb"))
+         storage_type == std::string("usb"))
     {
      sd_device_get_devname(device, &dev_name);
      device_devname_list.push_back(dev_name);
     }
   }
+
+}
+
+void find_mounted_devices(const std::vector<std::string>& usb_device_devname_list, 
+	std::vector<std::string>& usb_device_mounted_path_list)
+{
   std::ifstream mount_list_handler;
   std::string mount_device;
-  bool device_is_found = false;
 
   mount_list_handler.open("/proc/mounts");
   if(mount_list_handler.is_open())
   {
    while(getline(mount_list_handler, mount_device))
    {
-    for(auto name : device_devname_list)
+    for(auto name : usb_device_devname_list)
     {
-     if(name ==
-	mount_device.substr(int(0), mount_device.find_first_of(" ")))
+     if(name == mount_device.substr(int(0), mount_device.find_first_of(" ")))
      {
-      device_is_found = true;
       auto path = mount_device.substr(mount_device.find_first_of(" ") + int(1));
-      std::cout << path.substr(int(0), path.find_first_of(" "))  << std::endl;
+      usb_device_mounted_path_list.push_back(path.substr(int(0), path.find_first_of(" ")));
      }
     }
    }
@@ -59,7 +61,33 @@ int main()
   {
     std::cerr << "Unable to open the mount list" << std::endl;
   }
- if(!device_is_found) std::cout << "No usb storage is found" << std::endl;
+
+//check for unmounted devices
+ while(usb_device_mounted_path_list.size() < usb_device_devname_list.size())
+ {
+  usb_device_mounted_path_list.push_back("Unmounted");
+ }
+}
+
+int main()
+{
+ std::vector<std::string> usb_devname_list;
+ std::vector<std::string> usb_mount_list;
+
+ get_usb_storage_devname_list(usb_devname_list);
+ find_mounted_devices(usb_devname_list, usb_mount_list);
+
+ if(!usb_devname_list.empty())
+ {
+  for(auto& path : usb_mount_list)
+  {
+   std::cout << path << std::endl;
+  }
+ }
+ else
+ {
+  std::cout << "No usb storage is found" << std::endl;
+ }
 
   return 0;
 }
